07866f122e45d46d6e7fe7cc50238b2aa883420d
Todd Burry <todd@vanillaforums.com>
2019-08-24 11:19:05 -0400

    Update build for nightly

--- tests/AbstractCliTest.php
+++ tests/AbstractCliTest.php
@@ -21,23 +21,23 @@ abstract class AbstractCliTest extends TestCase {
     protected function setUp() {
         $this->errors = [];
         $this->expectErrors = false;
-        set_error_handler(function ($errno, $errstr, $errfile, $errline, $errcontext) {
+        set_error_handler(function ($errno, $message, $file, $line) {
             $reporting = error_reporting();
             if ($this->expectErrors) {
-                $this->errors[] = compact("errno", "errstr", "errfile", "errline", "errcontext");
+                $this->errors[] = compact("errno", "message", "file", "line");
             } elseif (error_reporting() !== 0) {
                 switch ($errno) {
                     case E_NOTICE:
                     case E_USER_NOTICE:
                     case E_STRICT:
-                        throw new \PHPUnit_Framework_Error_Notice($errstr, $errno, $errfile, $errline);
+                        throw new \PHPUnit_Framework_Error_Notice($message, $errno, $file, $line);
                         break;
                     case E_WARNING:
                     case E_USER_WARNING:
-                        throw new \PHPUnit_Framework_Error_Warning($errstr, $errno, $errfile, $errline);
+                        throw new \PHPUnit_Framework_Error_Warning($message, $errno, $file, $line);
                         break;
                     default:
-                        throw new \PHPUnit_Framework_Error($errstr, $errno, $errfile, $errline);
+                        throw new \PHPUnit_Framework_Error($message, $errno, $file, $line);
                 }
             }
         });
